66a11f9ee3b8c46ddc3c69b6019d295b2f13a8f4
ADILSE2029 <adilamiinn@gmail.com>
2026-02-21 15:49:40 +0500

    View Category Fixed and improved,other improvements

--- module1.py
+++ module1.py
@@ -1,28 +1,39 @@
-class brokebuster:
+import os
+
+
+class BrokeBuster:
+    def __init__(self):
+        self.filename = "brokebuster.txt"
+
     def add_expense(self):
+        if not os.path.exists(self.filename):
+            with open(self.filename, "w") as f:
+                f.write("")
+            print("No Data Storage File was found,Created Data Storage File.")
+            return
+        with open(self.filename, "r") as f:
+            fetched_data = f.readlines()
 
-        with open("brokebuster.txt", "r") as f:
-            checker = f.read()
-            if not checker:
-                decorator(60)
-                print("No Category Found,Add Category using main menu")
-            else:
+        given_category = input("Write Category:").strip()
+        decorator(60)
+
+        found = False
+        for i, data in enumerate(fetched_data):
+            broked_data = data.strip().split('|')
+            category_name = broked_data[0].strip()
+            category_spendings = int(broked_data[1])
+            if category_name == given_category:
+                found = True
                 given_expense = int(input("Write Expense:"))
-                given_category = input("Write Category:")
-                f.seek(0)
-                fetched_data = f.readlines()
-                for i, data in enumerate(fetched_data):
-                    broked_data = data.strip().split('|')
-                    category_name = broked_data[0]
-                    category_spendings = int(broked_data[1])
-                    if category_name == given_category:
-                        category_spendings += given_expense
-                        fetched_data[i] = f"{category_name}|{category_spendings}\n"
-                        print(
-                            f"Expense Added. New Expense for category {category_name} : {category_spendings}\n")
-
-                with open("brokebuster.txt", "w") as f:
-                    f.writelines(fetched_data)
+                category_spendings += given_expense
+                fetched_data[i] = f"{category_name}|{category_spendings}\n"
+                print(
+                    f"{category_spendings}$ Added  to category {category_name}\n")
+        if not found:
+            print("No Category Found\nuse main menu to add category")
+            decorator(60)
+        with open(self.filename, "w") as f:
+            f.writelines(fetched_data)
 
     def add_category(self):
         decorator(60)
@@ -44,7 +55,7 @@ class brokebuster:
                 category_price = "Spendings"
                 print(f"{category_name:<30}{category_price}")
                 for data in fetched_data:
-                    data_broked = fetched_data[0].strip().split('|')
+                    data_broked = data.strip().split('|')
                     category_name = data_broked[0]
                     category_saved_amount = data_broked[1]
                     decorator(60)
